db170eea4f1750e711b50888f0128ed03c281d4a
Cesar <guto_montaldi@yahoo.com.br>
2024-03-23 15:25:01 -0300

    Editando cadastros.

--- src/main/java/bean/UsuarioBean.java
+++ src/main/java/bean/UsuarioBean.java
@@ -1,5 +1,8 @@
 package bean;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
@@ -12,12 +15,22 @@ public class UsuarioBean {
 	
 	private Usuario usuario = new Usuario();
 	private DaoGeneric<Usuario> daoGeneric = new DaoGeneric<>();
+	private List<Usuario> usuarios = new ArrayList<Usuario>();
 	
 	public Usuario getUsuario() {
 		return usuario;
 	}
 	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
+	}
+	
+	public List<Usuario> getUsuarios() {
+		usuarios = daoGeneric.listar(Usuario.class);
+		return usuarios;
+	}
+	
+	public void setUsuarios(List<Usuario> usuarios) {
+		this.usuarios = usuarios;
 	}
 	
 	public String salvar() {
@@ -28,5 +41,11 @@ public class UsuarioBean {
 	public String novo() {
 		usuario = new Usuario();
 		return "";
+	}
+	
+	public List<Usuario> buscarUsuarios() {
+		
+		usuarios = daoGeneric.listar(Usuario.class);
+		return usuarios;
 	}
 }
